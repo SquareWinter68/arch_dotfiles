a89b59ea94749863189c083c44097f6fd2cf90ef
SquareWinter68 <sqarewinter@gmail.com>
2024-02-22 19:38:16 +0100

    Added microphone mute shortcut
    Eddited the volume_controll.sh script in order to add functionallity for toggeling microphone mute with f4 key, and the corresponding icons to show

--- hypr/scripts/volume_controll.sh
+++ hypr/scripts/volume_controll.sh
@@ -1,4 +1,6 @@
 sinks=$(pactl list sinks short | awk '{print $1}')
+sources=$(pactl list sources short | awk '{print $1}')
+
 
 case $1 in
     up)
@@ -39,21 +41,34 @@ case $1 in
             pactl set-sink-mute $sink 1
         done
         ;;
+    mute_mic)
+        for source in $sources; do
+            pactl set-source-mute $source toggle
+        done
+        ;;
 esac
 
 volume=$(pactl list sinks | grep 'Volume: front-left' | awk '{sum += $5} END {print sum/NR}' | sed 's/%//')
 
+get_microphone_state() {
+    mic_muted=$(pactl list sources | grep -A10 0 | grep -m1 'Mute:' | awk '{print $2}')
+    echo $mic_muted
+}
 
 send_notification() {
-	if [ "$1" = "mute" ] || [ "$volume" -eq 0 ]; then ICON="$HOME/.config/dunst/icons/volume-xmark-solid.svg"; elif [ "$volume" -lt 33 ]; then ICON="$HOME/.config/dunst/icons/volume-off-solid.svg"; elif [ "$volume" -lt 66 ]; then ICON="$HOME/.config/dunst/icons/volume-low-solid.svg"; else ICON=$HOME/.config/dunst/icons/volume-high-solid.svg; fi
+	if [ "$1" = "mute" ] || [ "$volume" -eq 0 ]; then ICON="$HOME/.config/dunst/icons/volume-xmark-solid.svg"; elif [ "$1" = "mute_mic" ] && [ "$mic_muted" = "yes" ]; then ICON="$HOME/.config/dunst/icons/microphone-slash-solid.svg"; elif [ "$1" = "mute_mic" ] && [ "$mic_muted" = "no" ]; then ICON="$HOME/.config/dunst/icons/microphone-solid.svg"; elif [ "$volume" -lt 33 ]; then ICON="$HOME/.config/dunst/icons/volume-off-solid.svg"; elif [ "$volume" -lt 66 ]; then ICON="$HOME/.config/dunst/icons/volume-low-solid.svg"; else ICON=$HOME/.config/dunst/icons/volume-high-solid.svg; fi
 	if [ "$1" = "mute" ]; then TEXT="Currently muted"; else TEXT="Currently at ${volume}%"; fi
 
 	dunstify -a "Volume" -r 1415 -h int:value:"$volume" -i "$ICON" "Volume" -t 2000
 }
 
+
 case $1 in
 	mute)
 		send_notification mute;;
+    mute_mic)
+    get_microphone_state
+        send_notification mute_mic;;    
 	*)
 		send_notification;;
 esac
